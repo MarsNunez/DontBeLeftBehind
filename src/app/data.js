3f8a4925b067ef48b7db7d6041a89d49d91729c9
MarsNunez <mars.nunez.arenas@gmail.com>
2024-06-11 22:53:51 -0500

    adding button

---
+++ src/app/data.js
@@ -0,0 +1,34 @@
+export const goals = [
+  {
+    value: "weight-loss",
+    title: "Weight loss",
+    summarizing:
+      "Focusing on reducing body fat and achieving a healthy weight.",
+    emoji: "🏋️",
+  },
+  {
+    value: "muscle-building",
+    title: "Muscle building",
+    summarizing: "Aiming to increase muscle size and strength.",
+    emoji: "💪",
+  },
+  {
+    value: "overall-fitness",
+    title: "Overall fitness",
+    summarizing: "Enhancing cardiovascular health, endurance, and flexibility.",
+    emoji: "🏃‍♂️",
+  },
+  {
+    value: "muscle-definition",
+    title: "Muscle definition",
+    summarizing:
+      "Improve muscle definition and achieve a leaner, more sculpted physique.",
+    emoji: "💯",
+  },
+  {
+    value: "mental-health",
+    title: "Mental health",
+    summarizing: "Using exercise to reduce stress and improve mood.",
+    emoji: "🧠",
+  },
+];
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,61 @@
+"use client";
+import { goals } from "./data";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [goalSelection, setGoalSelection] = useState("");
   return (
-    <main className="py-10">
+    <main className="py-10 max-w-5xl mx-auto">
       <h1 className="text-center font-semibold text-3xl">
-        Dont Be Left Behind 💪
+        Don't Be Left Behind ⏳
       </h1>
+      <p className="mt-16 mb-5">
+        Ok, first things first, select what is your goal:
+      </p>
+      <div className="grid grid-cols-3 gap-10">
+        {goals.map((goal) => (
+          <div
+            className={`border-2 rounded-2xl ${
+              goalSelection == goal.value && "border-green-600 bg-green-100"
+            } py-5 px-3 flex flex-col items-center justify-center`}
+            onClick={() => setGoalSelection(goal.value)}
+            key={goal.value}
+          >
+            <div className="text-center">
+              <p className="text-5xl">{goal.emoji}</p>
+              <p className="font-medium my-3">{goal.title}</p>
+              <p className="text-sm">{goal.summarizing}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <p className="mt-16 mb-5">
+        Now select all of your ingredients around your zone you can buy or just
+        you like the most for creating your ultimate diet to reach your GOAL 🎯
+      </p>
+
+      <div className="checkbox-group border w-fit">
+        <label>Meat</label> <br />
+        <label className="checkbox-label">
+          <input type="checkbox" id="option1" />
+          <span className="custom-checkbox"></span>
+          Option 1
+        </label>
+        <br />
+        <label className="checkbox-label">
+          <input type="checkbox" id="option2" />
+          <span className="custom-checkbox"></span>
+          Option 2
+        </label>
+        <br />
+        <label className="checkbox-label">
+          <input type="checkbox" id="option3" />
+          <span className="custom-checkbox"></span>
+          Option 3
+        </label>
+      </div>
     </main>
   );
 }
